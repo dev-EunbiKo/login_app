a0f3227dc9f7305e038b2a2b9d0f378684514090
dev_ebko <dev.ebko.ved@gmail.com>
2025-07-03 14:51:06 +0900

    refactoring - custom container

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:login_app/widget/my_container.dart';
 import 'package:login_app/widget/my_textfield.dart';
 
 class Login extends StatefulWidget {
@@ -52,18 +53,13 @@ class _LoginState extends State<Login> {
               const SizedBox(height: 25),
               MyTextfield(hintText: "Password", obscureText: true),
               const SizedBox(height: 25),
-              Container(
+              MyContainer(
                 width: 120,
                 height: 50,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.orange[300],
-                ),
-                child: const Icon(
-                  Icons.arrow_forward,
-                ).animate().fade(delay: 1000.ms),
-              ),
+                backgroundColor: Colors.orange[200],
+                radius: 20,
+                icon: const Icon(Icons.arrow_forward),
+              ).animate().fade(delay: 1000.ms),
               const SizedBox(height: 20),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -106,18 +102,12 @@ class _LoginState extends State<Login> {
                     ],
                   ),
                   const SizedBox(width: 10),
-                  Container(
+                  MyContainer(
                         width: 50,
                         height: 50,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.white),
-                          borderRadius: BorderRadius.circular(16),
-                          color: Colors.grey[200],
-                          image: const DecorationImage(
-                            image: AssetImage('images/codingchef3.png'),
-                            fit: BoxFit.fill,
-                          ),
-                        ),
+                        backgroundColor: Colors.grey[300],
+                        radius: 16.0,
+                        imagePath: 'images/codingchef3.png',
                       )
                       .animate()
                       .slideX(begin: 3, end: 0, duration: 300.ms)
